d53334e74c68828377a278124e4e28cb5fc1d1dc
Trofiproll <denispartola@gmail.com>
2024-01-26 10:40:37 +1000

    Добавлены экраны для трёх кнопок и переход к ним

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.activity_main)
+
+        val searchBtn = findViewById<Button>(R.id.search_btn)
+        val mediathekaBtn = findViewById<Button>(R.id.mediatheka_btn)
+        val settingsBtn = findViewById<Button>(R.id.settings_btn)
+
+
+
+        searchBtn.setOnClickListener{
+            startActivity(Intent(this, SearchActivity::class.java))
+        }
+        mediathekaBtn.setOnClickListener{
+            startActivity(Intent(this, MediathekaActivity::class.java))
+        }
+        settingsBtn.setOnClickListener{
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backBtn = findViewById<ImageView>(R.id.settings_back_btn)
+        backBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
